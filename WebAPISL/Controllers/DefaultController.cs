51e18ff15a1d164c9efad11d459276bf73f6b8eb
Rohitha Koduri <rohithakoduri@gmail.com>
2021-08-02 15:29:41 +0530

    webAPI

---
+++ WebAPISL/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPISL.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        //Post
+        public string Post( [FromBody]string value){
+                return "Post";
+            }
+        //Put
+        public void Put(int id,[FromBody]string value)
+        {
+
+        }
+    }
+}
---
+++ WebAPISL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPISL.WebApiApplication" Language="C#" %>
